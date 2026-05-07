8d42986cefc2b93e5cca56f22456e8d517a53e92
telumahesh-Naidu <telumahesh7@gmail.com>
2026-05-07 20:33:10 +0530

    React day 1

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+const App=()=>{
+  return(
+    <p>
+      <h1>Welcome to the react js</h1>
+      <ol>
+        <li>list-1</li>
+        <li>list-2</li>
+        <li>list-3</li>
+      </ol>
+      <h2>hello</h2>
+    </p>
+  )
+}
+export default App;
